fec5f5a576285e0d0e4304daedaa6168086e51c7
By_XChronoX <89328981+XChronoX@users.noreply.github.com>
2024-05-21 13:28:15 -0500

    Create Reproductor.java

---
+++ Spotify/Reproductor.java
@@ -0,0 +1,27 @@
+package Spotify;
+
+import java.util.ArrayList;
+
+public class Reproductor {
+
+    ArrayList<Cancion> reproductor;
+     static Boolean reproduciendo;
+
+    
+    public Reproductor(){
+        this.reproduciendo = false;
+
+    }
+
+    public static void Reproducir(){
+        
+        reproduciendo=true;
+
+    }
+
+    public static Boolean Reproduciendo(){
+
+        return reproduciendo;
+    }
+
+}
